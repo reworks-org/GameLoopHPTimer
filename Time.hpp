76a6a913a2cddc07c1a682a9976f40a788005fc6
Domenic <student@Dom.local>
2015-03-15 15:57:05 +0800

    final commit
    fixed up readme and inlined the functions

--- Time.hpp
+++ Time.hpp
@@ -20,15 +20,16 @@ class ChronoWrapper //rename this if you want. i couldnt think of anything bette
     typedef system_clock SC; //also adds this if your using chrono for other things aswell.
     
 public:
+    //we use an unsigned 64bit integer because its the largest interger to use for storing these huge numbers
     //returns the current system time in nanoseconds (use for gameloops over functions like SDL_GetTicks() ). This is the same as Java's System.nanoTime()
-    std::uint64_t nanoTime()
+    inline std::uint64_t nanoTime()
     {
         //get current time since epoch from curTime and return in nanoseconds
         return duration_cast<nanoseconds>(HRC::now().time_since_epoch()).count();
     }
     
     //returns time since 1970 in milliseconds. This is more accurate than <ctime>'s time() which returns in seconds. This is the same as Java's Date().getTime() or System.currentTimeMillis()
-    std::uint64_t sysTimeMillis()
+    inline std::uint64_t sysTimeMillis()
     {
         //cast to milliseconds and return
         return duration_cast<milliseconds>(SC::now().time_since_epoch()).count();
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 Small wrapper around std::chrono that implements some needed functions more easily. It basically emulates javas System.NanoTime() and System.GetTimeMillis()
 
-Note: Requires a C++11 compliant compiler that supports <chrono> and the 'auto' typedef.
+Note: Requires a C++11 (C++0x) compliant compiler that supports <chrono> and the 'auto' type.
