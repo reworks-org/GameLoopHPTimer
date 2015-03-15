63b56f921625e2829d86bca138361c0b6d1eb614
Domenic <student@Dom.local>
2015-03-15 15:53:11 +0800

    final version optimised code

--- Time.hpp
+++ Time.hpp
@@ -4,6 +4,7 @@
 //
 //  Single header for everything. Just add to your project and away you go!
 
+//inclusion guards. use #pragma once if you prefer that...
 #ifndef __TIME__CHRONO__
 #define __TIME__CHRONO__
 
@@ -12,30 +13,25 @@
 
 using namespace std::chrono; //to keep things clean. you can remove if you want, but prepare to have to add it to all the lines of code.
 
-class ChronoWrapper
+class ChronoWrapper //rename this if you want. i couldnt think of anything better.
 {
     //defines a type Chrono of namespace std::chrono::high_resolution_clock (nanosecond timer)
-    typedef high_resolution_clock Chrono;
+    typedef high_resolution_clock HRC;
+    typedef system_clock SC; //also adds this if your using chrono for other things aswell.
     
 public:
     //returns the current system time in nanoseconds (use for gameloops over functions like SDL_GetTicks() ). This is the same as Java's System.nanoTime()
     std::uint64_t nanoTime()
     {
-        //define type at compiletime. set it to the current system time
-        auto curTime = Chrono::now();
-        
         //get current time since epoch from curTime and return in nanoseconds
-        return duration_cast<nanoseconds>(curTime.time_since_epoch()).count();
+        return duration_cast<nanoseconds>(HRC::now().time_since_epoch()).count();
     }
     
     //returns time since 1970 in milliseconds. This is more accurate than <ctime>'s time() which returns in seconds. This is the same as Java's Date().getTime() or System.currentTimeMillis()
     std::uint64_t sysTimeMillis()
     {
-        //get current time from system clock
-        auto curTime = system_clock::now().time_since_epoch();
-        
         //cast to milliseconds and return
-        return duration_cast<milliseconds>(curTime).count();
+        return duration_cast<milliseconds>(SC::now().time_since_epoch()).count();
     }
 };
 
